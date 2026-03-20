202e55780e2d8f1a21fad23b200b2ab8fc9dd68b
Helena <ux24ii401@ux.edu.mx>
2026-03-20 07:45:15 -0600

    Practica04

---
+++ src/main/java/com/mx/curso/unidad2/practica04/Main.java
@@ -0,0 +1,17 @@
+package com.mx.curso.unidad2.practica04;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SensorIA[] sensores = new SensorIA[3];
+
+        sensores[0] = new SensorLidar();
+        sensores[1] = new SensorVision();
+        sensores[2] = new SensorUltrasonido();
+
+        for (SensorIA s : sensores) {
+            s.leerDatos();
+        }
+    }
+}
---
+++ src/main/java/com/mx/curso/unidad2/practica04/SensorUltrasonido.java
@@ -0,0 +1,9 @@
+package com.mx.curso.unidad2.practica04;
+
+public class SensorUltrasonido extends SensorIA {
+
+    @Override
+    public void leerDatos() {
+        System.out.println("Ultrasonido leyendo datos");
+    }
+}
