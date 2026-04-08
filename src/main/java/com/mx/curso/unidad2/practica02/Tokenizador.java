d159063cb33f0e7ba3a7b6c48997c83af1d0cc67
Helena <ux24ii401@ux.edu.mx>
2026-04-08 07:27:31 -0600

    ejercicio 4/8/26

---
+++ src/main/java/com/mx/curso/unidad2/practica02/Tokenizador.java
@@ -0,0 +1,6 @@
+package com.mx.curso.unidad2.practica02;
+
+public class Tokenizador {
+
+    public static void dividirTexto(String parrafo){}
+}
---
+++ src/main/java/com/mx/curso/unidad2/practica02/TokenizadorIMPL.java
@@ -0,0 +1,4 @@
+package com.mx.curso.unidad2.practica02;
+
+public class TokenizadorIMPL {
+}
